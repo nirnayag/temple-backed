5d0f5cb5f223d2024d46b3f2694815638ce57d83
nirnay8 <nirnaysawant.primetech@gmail.com>
2025-05-18 17:40:16 +0530

    Add TypeScript definitions and configuration files for Twilio and Day.js plugins

--- models/User.js
+++ models/User.js
@@ -1,23 +1,24 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
     unique: true,
+    sparse: true,
     trim: true
   },
   email: {
     type: String,
-    required: true,
     unique: true,
+    sparse: true,
     trim: true,
     lowercase: true
   },
-  password: {
+  mobileNumber: {
     type: String,
-    required: true
+    required: true,
+    unique: true,
+    trim: true
   },
   role: {
     type: String,
@@ -31,28 +32,16 @@ const userSchema = new mongoose.Schema({
   isActive: {
     type: Boolean,
     default: true
+  },
+  otp: {
+    type: String
+  },
+  otpExpiry: {
+    type: Date
   }
 }, {
   timestamps: true
 });
-
-// Hash password before saving
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
-
-// Method to compare password
-userSchema.methods.comparePassword = async function(candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
-};
 
 const User = mongoose.model('User', userSchema);
 
